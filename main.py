fe96e76b94c28156800ad372c754c90c876c65ef
dr.number <dr.number@yandex.ru>
2022-10-13 13:05:28 +0700

    update

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
 
 @app.route("/")
 def start():
-    return "<p>Hello, flask!</p>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    #app.run(host="0.0.0.0")
     app.run(port=5000)
+
+    db.create_all()    
