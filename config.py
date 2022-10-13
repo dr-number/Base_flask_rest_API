e4ed2232236ba086ab191ff185d2d601003c8347
dr.number <dr.number@yandex.ru>
2022-10-13 13:54:15 +0700

    update

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+    __BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    __BASE_NAME = 'db.sqlite'
+
+    BASE_PATH = f'sqlite:///{os.path.join(__BASE_DIR, __BASE_NAME)}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
+from crypt import methods
+from config import Config
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URL'] = Config.BASE_PATH
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
 
-@app.route("/")
+
+@app.route("/", methods=["get"])
 def start():
-    return render_template('index.html')
+    return jsonify({ 'status': 'API working...' })
 
 if __name__ == '__main__':
     app.run(port=5000)
-
-    db.create_all()    
